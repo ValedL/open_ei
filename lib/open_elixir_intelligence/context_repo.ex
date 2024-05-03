90855b0c396332bb24b0c5bb886f845f295b4ab9
luzinv <luzinv@gmail.com>
2024-05-03 14:21:55 +0300

    fix: update correct path

--- lib/open_elixir_intelligence/context_repo.ex
+++ lib/open_elixir_intelligence/context_repo.ex
@@ -8,8 +8,8 @@ defmodule OpenElixirIntelligence.ContextRepo do
     "lib/open_elixir_intelligence/text_extractor.ex",
     "lib/open_elixir_intelligence/very_bad_code.ex",
     "lib/open_elixir_intelligence/divide_by_zero.ex",
-    "lib/open_elixir_intelligence/live/open_ei_portal.ex",
-    "lib/open_elixir_intelligence/live/open_eai_portal.ex"
+    "lib/open_elixir_intelligence_web/live/open_ei_portal.ex",
+    "lib/open_elixir_intelligence_web/live/open_eai_portal.ex"
   ]
 
   def load_context() do
